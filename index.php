cea50c1e8cd35ffea7bdd2956c8a99fa84f14ff4
Luke Marshall <luke.w.marshall@student.shu.ac.uk>
2024-05-23 00:32:10 +0100

    add logic for displaying recommendation on the same page

--- index.php
+++ index.php
@@ -7,10 +7,34 @@
 </head>
 <body>
     <h1>FLIP THE TORIES</h1>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        require_once('./db_connect.php');
+        require_once('./recommendation.php');
+
+        $databaseConnector = new DatabaseConnector(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        $databaseConnector->connect();
+        $dbc = $databaseConnector->getConnection();
+
+        $recommender = new Recommender($dbc);
+        $constituency = $_POST["constituency"];
+        $recommender->getRecommendation($constituency);
+    
+    echo "<p>Please enter your constituency:</p>
+    <form action='' method='post'>
+        <input type='text' name='constituency' placeholder='Enter your constituency'>
+        <button type='submit'>Get Recommendation</button>
+    </form>";
+    } else {
+    ?>
+    
     <p>Please enter your constituency:</p>
-    <form action="recommendation.php" method="post">
+    <form action="" method="post">
         <input type="text" name="constituency" placeholder="Enter your constituency">
         <button type="submit">Get Recommendation</button>
     </form>
+    <?php
+    }
+    ?>
 </body>
 </html>
